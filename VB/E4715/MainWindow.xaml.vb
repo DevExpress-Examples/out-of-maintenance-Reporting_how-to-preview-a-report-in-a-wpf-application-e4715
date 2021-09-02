cf43ea5d00cbb83553e27c5942e091ac6796f224
DevExpressExampleBot <fakeEmail>
2021-09-02 19:22:37 +0300

    Source auto update [skip ci]

--- VB/E4715/MainWindow.xaml.vb
+++ VB/E4715/MainWindow.xaml.vb
@@ -14,19 +14,19 @@ Imports System.Windows.Shapes
 Imports DevExpress.Xpf.Printing
 
 Namespace E4715
-    ''' <summary>
-    ''' Interaction logic for MainWindow.xaml
-    ''' </summary>
-    Partial Public Class MainWindow
-        Inherits Window
+	''' <summary>
+	''' Interaction logic for MainWindow.xaml
+	''' </summary>
+	Partial Public Class MainWindow
+		Inherits Window
 
-        Public Sub New()
-            InitializeComponent()
-        End Sub
+		Public Sub New()
+			InitializeComponent()
+		End Sub
 
-        Private Sub button1_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
-            Dim report As New XtraReport1()
-            PrintHelper.ShowPrintPreview(Me, report)
-        End Sub
-    End Class
+		Private Sub button1_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			Dim report As New XtraReport1()
+			PrintHelper.ShowPrintPreview(Me, report)
+		End Sub
+	End Class
 End Namespace
